adaaf63a48158d7375cf34af44fdf7c08a8d8d7c
BorsosIstvan <bistvanus@gmail.com>
2026-06-27 00:10:43 +0200

    db

--- hj3_get_next_song.php
+++ hj3_get_next_song.php
@@ -21,7 +21,7 @@ function haalWillekeurigNummer($db) {
     if (empty($preview_url)) {
         $schone_artiest = str_replace('&', ' ', $song['artist']);
         $zoekterm = urlencode($schone_artiest . " " . $song['title']);
-        $api_url = "https://apple.com" . $zoekterm . "&limit=1&entity=song";
+        $api_url = "https://itunes.apple.com/search?term=" . $zoekterm . "&limit=1&entity=song";
 
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $api_url);
