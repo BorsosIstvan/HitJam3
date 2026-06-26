6a2e2f406b81f75265a8e399025511ce00ef69a7
BorsosIstvan <bistvanus@gmail.com>
2026-06-27 00:34:02 +0200

    db

--- hj3_get_next_song.php
+++ hj3_get_next_song.php
@@ -29,7 +29,7 @@ try {
         // Maak de zoekterm schoon voor de URL
         $schone_artiest = str_replace('&', ' ', $song['artist']);
         $zoekterm = urlencode($schone_artiest . " " . $song['title']);
-        $api_url = "https://apple.com" . $zoekterm . "&limit=1&entity=song";
+        $api_url = "https://itunes.apple.com/search?term=" . $zoekterm . "&limit=1&entity=song";
 
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $api_url);
